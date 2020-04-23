898ebff44c2a5b202d8e0af33012c03d5b90532e
Amir <amirbakarov@gmail.com>
2020-04-23 13:40:56 +0300

    add result logging to dataframe

--- experiments/solvers.py
+++ experiments/solvers.py
@@ -2,13 +2,15 @@ import numpy as np
 from scipy.spatial import distance
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
-from typing import List
+from typing import List, Dict
+from pandas import DataFrame
 
 
 class Solver:
-    def __init__(self, clf, metric):
+    def __init__(self, clf, metric, vectors_list: List):
         self.clf = clf
         self.metric = metric
+        self.result_table = DataFrame(index=vectors_list)
 
     def concat_vectors(self, vectors_1: List, vectors_2: List) -> np.ndarray:
         return np.array(
@@ -38,10 +40,19 @@ class Solver:
         vectors = self.cosine_vectors(vectors_1, vectors_2)
         if task == 'nli':
             labels = self.encode_labels(labels_)
-        else if task == 'sts':
+        elif task == 'sts':
             labels = labels_
         self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(vectors, labels, test_size=0.1,
                                                                                 random_state=42)
         self.fit()
         self.predict()
         return self.get_result()
+    
+    def conduct_experiment(self, vectors_dict: Dict, labels: List, task: str, experiment_name: str):
+        results = []
+        for vectors_name, vectors in vectors_dict.items():
+            results.append(self.evaluate(vectors[0], vectors[1], labels, task))
+        self.result_table[experiment_name] = results
+        
+    def get_result_table(self):
+        return self.result_table
